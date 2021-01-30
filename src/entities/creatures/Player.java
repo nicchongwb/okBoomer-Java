d90dd362facbdce86bde5c2e610acbdc69164aaa
BestCrochet <alicia159@live.com.sg>
2021-01-30 12:45:02 +0800

    Added player collision and map collision, and set up skeleton for item interaction
    Summary:
    -> added player collision to prevent player from exiting the map
    -> added player collision to prevent player from colliding into each other
    
    Player.java -> added codes for collision
    GameState.java -> added several methods for detecting player collision, map collision and backbone for item interaction

--- src/entities/creatures/Player.java
+++ src/entities/creatures/Player.java
@@ -2,6 +2,8 @@ package entities.creatures;
 
 import gfx.Assets;
 import okBoomer.Game;
+import states.GameState;
+import worlds.World;
 
 import java.awt.*;
 
@@ -13,6 +15,9 @@ GameState render() -> player.render(g);
  */
 
 public class Player extends Creature{
+    // Access current board information about player1/2 currX, currY from GameState static variables
+    private World world = GameState.getWorld();
+
     private static int playerCount = 0;
     private static int pixToMove = 32; // Amount of pixels to move
 
@@ -24,6 +29,10 @@ public class Player extends Creature{
     // declare variables to check if key is already pressed
     private static boolean alrPressedp1 = false;
     private static boolean alrPressedp2 = false;
+
+    // temporary variables to hold next player coordinates
+    private static int newX;
+    private static int newY;
 
     public Player(Game game, int x, int y) {
         super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
@@ -45,12 +54,26 @@ public class Player extends Creature{
         xMove = 0;
         yMove = 0;
 
+        newX = x;
+        newY = y;
+
         if (pid == 0){ // If pid == 0 for player 1
             if (game.getKeyManager().p1Up) { // if keyPressed is true
 
                 if(!alrPressedp1){ // check if alrPressed is true
-                    yMove = -speed;
-                    //System.out.printf("P1: %d\tY: %d%n", x, y);
+
+                    // for player collision purposes, check if the next tile the player will be moving to
+                    // collides with the edges of the map.
+                    newY = y - speed;
+
+                    // if they don't collide, allow them to move
+                    // and update board array
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        yMove = -speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
                     alrPressedp1 = true; // set alrPressed to true so our player won't move themselves continuously
                                         // Note: alrPressed is set to false inside KeyManager.java on keyRelease
                 }
@@ -58,8 +81,14 @@ public class Player extends Creature{
             if (game.getKeyManager().p1Down) {
 
                 if(!alrPressedp1){
-                    yMove = speed;
-                    //System.out.printf("P1: %d\tY: %d%n", x, y);
+
+                    newY = y + speed;
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        yMove = speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
                     alrPressedp1 = true;
                 }
 
@@ -67,8 +96,15 @@ public class Player extends Creature{
             if (game.getKeyManager().p1Left) {
 
                 if(!alrPressedp1){
-                    xMove = -speed;
-                    //System.out.printf("P1: %d\tY: %d%n", x, y);
+
+                    newX = x - speed;
+
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        xMove = -speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
                     alrPressedp1 = true;
                 }
 
@@ -76,19 +112,33 @@ public class Player extends Creature{
             if (game.getKeyManager().p1Right) {
 
                 if(!alrPressedp1){
-                    xMove = speed;
-                    //System.out.printf("P1: %d\tY: %d%n", x, y);
+
+                    newX = x + speed;
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        xMove = speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
                     alrPressedp1 = true;
                 }
 
             }
         }
         else if (pid == 1){ // It pid == 1 for player 2
+
             if (game.getKeyManager().p2Up) {
 
                 if(!alrPressedp2){
-                    yMove = -speed;
-                    //System.out.printf("P2: %d\tY: %d%n", x, y);
+                    newY = y - speed;
+
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        yMove = -speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
+
                     alrPressedp2 = true;
                 }
 
@@ -96,8 +146,16 @@ public class Player extends Creature{
             if (game.getKeyManager().p2Down) {
 
                 if(!alrPressedp2){
-                    yMove = speed;
-                    //System.out.printf("P2: %d\tY: %d%n", x, y);
+
+                    newY = y + speed;
+
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        yMove = speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
+
                     alrPressedp2 = true;
                 }
 
@@ -105,8 +163,15 @@ public class Player extends Creature{
             if (game.getKeyManager().p2Left) {
 
                 if(!alrPressedp2){
-                    xMove = -speed;
-                    //System.out.printf("P2: %d\tY: %d%n", x, y);
+
+                    newX = x - speed;
+
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        xMove = -speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
                     alrPressedp2 = true;
                 }
 
@@ -114,8 +179,15 @@ public class Player extends Creature{
             if (game.getKeyManager().p2Right) {
 
                 if(!alrPressedp2){
-                    xMove = speed;
-                    //System.out.printf("P2: %d\tY: %d%n", x, y);
+
+                    newX = x + speed;
+
+                    if(GameState.canPlayerMove(pid, prevX, prevY, newX, newY)){ // coords are passed in as pixels
+                        xMove = speed;
+                    }
+                    else{
+                        System.out.println("No move pls");
+                    }
                     alrPressedp2 = true;
                 }
             }
